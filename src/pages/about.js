0fe2ccb3a3c0f1ef2d94091ea9192e1c98b49ce1
Ahmet Tarik Comert <ahmettarik.comert@student.ap.be>
2021-12-27 20:43:37 +0100

    React Gatsby menu opgesteld

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Us">
+      <p>This website is about listing all the Anime Studios and the anime they made.</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
